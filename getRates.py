410afd3520e1def3441bd433320a6041bf1f8638
juan <>
2022-01-15 19:58:33 -0300

    minor fix

--- getRates.py
+++ getRates.py
@@ -28,42 +28,42 @@ payload = [{'symbol': 'BTCUSD_220325'}, {'symbol': 'ETHUSD_220325'},
 df = pd.DataFrame(payload)
 df['response'] = ''
 
-while True:
-    t = PrettyTable([
-                'Symbol',
-                'Mark',
-                'Index',
-                'Dias',
-                'TNA',
-                'TEA',
-                'Tasa Directa'])
-    t.align = 'r'
-    n=0
-    print(time.strftime('%Y-%m-%d %H:%M:%S', time.localtime()))
-    for i in df['symbol']:
-        param = {'symbol': df['symbol'][n]}
-        response = requests.request("GET", url = baseEndPoint+futuresPremiumIndex, params = param)
-        responses = pd.DataFrame(response.json(), index=[0])
-        iniAnio = (df.iloc[n]['symbol']).find("_") + 1
-        dias=(date(int('20'+df.iloc[n]['symbol'][iniAnio: iniAnio +2]), int(df.iloc[n]['symbol'][iniAnio + 2:iniAnio + 4]), int(df.iloc[n]['symbol'][iniAnio + 4:iniAnio + 6])) - date.today()).days  
-        markPrice = round(float(responses.iloc[0]['markPrice']),4)
-        indexPrice = round(float(responses.iloc[0]['indexPrice']),4)
-        tna = round(((markPrice / indexPrice)-1) * (365 / dias) * 100, 2)
-        tasaImplicita = round(((markPrice / indexPrice) ** (365 / dias) - 1) * 100, 2)
-        tasaDirecta = round(((markPrice / indexPrice)-1) * 100, 2)
+#while True:
+t = PrettyTable([
+            'Symbol',
+            'Mark',
+            'Index',
+            'Dias',
+            'TNA',
+            'TEA',
+            'Tasa Directa'])
+t.align = 'r'
+n=0
+print(time.strftime('%Y-%m-%d %H:%M:%S', time.localtime()))
+for i in df['symbol']:
+    param = {'symbol': df['symbol'][n]}
+    response = requests.request("GET", url = baseEndPoint+futuresPremiumIndex, params = param)
+    responses = pd.DataFrame(response.json(), index=[0])
+    iniAnio = (df.iloc[n]['symbol']).find("_") + 1
+    dias=(date(int('20'+df.iloc[n]['symbol'][iniAnio: iniAnio +2]), int(df.iloc[n]['symbol'][iniAnio + 2:iniAnio + 4]), int(df.iloc[n]['symbol'][iniAnio + 4:iniAnio + 6])) - date.today()).days  
+    markPrice = round(float(responses.iloc[0]['markPrice']),4)
+    indexPrice = round(float(responses.iloc[0]['indexPrice']),4)
+    tna = round(((markPrice / indexPrice)-1) * (365 / dias) * 100, 2)
+    tasaImplicita = round(((markPrice / indexPrice) ** (365 / dias) - 1) * 100, 2)
+    tasaDirecta = round(((markPrice / indexPrice)-1) * 100, 2)
+    t.add_row([
+        i, 
+            markPrice,
+            indexPrice,
+            dias, 
+            tna,
+            tasaImplicita,
+            tasaDirecta]
+        )
+    if n == len(payload) / 2 - 1:
         t.add_row([
-            i, 
-              markPrice,
-              indexPrice,
-              dias, 
-              tna,
-              tasaImplicita,
-              tasaDirecta]
-            )
-        if n == len(payload) / 2 - 1:
-            t.add_row([
-                '--------------'] * 7)
-        n+=1
-    print(t)
+            '--------------'] * 7)
+    n+=1
+print(t)
     #sleep(secondsSleep)
 
